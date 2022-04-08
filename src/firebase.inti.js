d8d8d4fc44120bdf0fdea4c85385e80d50c52b85
rabiul1399 <www.rabiulislam3808@gmail.com>
2022-04-08 12:11:54 -0500

    first commit

---
+++ src/firebase.inti.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJh4FXMWY5tSQg02XSnZjTmJl0UL0SVLg",
+  authDomain: "privete-integration.firebaseapp.com",
+  projectId: "privete-integration",
+  storageBucket: "privete-integration.appspot.com",
+  messagingSenderId: "321072020569",
+  appId: "1:321072020569:web:106e642aa369f3f459c548"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
